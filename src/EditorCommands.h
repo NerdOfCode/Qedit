b7e3c692008fb38d1d402c56016dd3d3ff241d98
Nate Wander <nerdofcode@gmail.com>
2025-05-23 16:42:52 -0500

    Add EditorCommands header file

---
+++ src/EditorCommands.h
@@ -0,0 +1,21 @@
+//
+// Created by W K on 4/25/25.
+//
+
+#ifndef EDITORCOMMANDS_H
+#define EDITORCOMMANDS_H
+
+#pragma once
+
+namespace EditorCommands {
+    // Commands
+    static const std::string WRITE = ":w";
+    static const std::string QUIT = ":q";
+    static const std::string WRITE_QUIT = ":wq";
+
+    // Responses
+    static const std::string WROTE_TO = "wrote: ";
+    static const std::string INVALID_FILENAME_MSG = "Invalid filename.";
+}
+
+#endif //EDITORCOMMANDS_H
